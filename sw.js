8908f62f874dd0015a21cba7f9e10d46c234114b
moazzamazmi333-ui <moazzamazmi333@gmail.com>
2026-02-17 18:07:33 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,9 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js')
-        .then(reg => console.log('Service Worker registered!', reg))
-        .catch(err => console.log('SW registration failed:', err));
-}
+// sw.js - Minimal for PWA install prompt
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request));
+});
+
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', () => self.clients.claim());
+
+console.log('BookGadi Service Worker ready!');
