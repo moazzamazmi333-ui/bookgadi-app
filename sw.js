878f57c33ef1df0b9f7c1752ac3cf36b5e72af1f
moazzamazmi333-ui <moazzamazmi333@gmail.com>
2026-02-17 17:51:50 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,5 @@
-const CACHE_NAME = 'bookgadi-v1';
-const urlsToCache = ['/'];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
-});
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js')
+        .then(reg => console.log('Service Worker registered!', reg))
+        .catch(err => console.log('SW registration failed:', err));
+}
